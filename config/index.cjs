494f8bbbb4caa3cc9397d5c7040f499e98c019a4
SanXiaoXing <87142318+SanXiaoXing@users.noreply.github.com>
2023-06-24 21:56:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,12 +14,12 @@ const USER_CONFIG = {
   APP_SECRET: 'b176e6ddcfab0f3937c29446a13df158',
 
   PROVINCE: '北京',
-  CITY: '海淀区',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'SanXiaoXing',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_cXM6TJkduGYjkM6pgNYWzqQjAU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
