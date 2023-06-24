5b55c292971e87361c8c821c16ff3a0455eeede3
SanXiaoXing <87142318+SanXiaoXing@users.noreply.github.com>
2023-06-24 22:10:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'b176e6ddcfab0f3937c29446a13df158',
 
-  PROVINCE: '北京',
-  CITY: '北京',
+  
 
   USERS: [
     {
@@ -24,6 +23,8 @@ const USER_CONFIG = {
       id: 'o_cXM6TJkduGYjkM6pgNYWzqQjAU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'EDHogCTMikWDMM0RFh87sxOalgQ4_Q0XhgRS6Z-YyLE',
+      PROVINCE: '北京',
+      CITY: '北京',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-02',
       festivals: [
@@ -48,6 +49,52 @@ const USER_CONFIG = {
       ],
     },
   ],
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '蔡文博',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
+      id: 'o_cXM6TeI7sGXQsRYt6LGJjz5CE4',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'EDHogCTMikWDMM0RFh87sxOalgQ4_Q0XhgRS6Z-YyLE',
+      // 所在省份或城市，也可以不填
+      province: '河南',
+      // 所在城市或县区
+      city: '安阳',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '12-27',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://wangxinleo.cn',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+        },
+        {
+          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+        }
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+        // 退伍日
+        { keyword: 'ex_day', date: '2022-09-10' }
+      ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
