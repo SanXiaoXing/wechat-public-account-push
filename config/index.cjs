fb779fe4cfcc11e8fd094ff732598e489f5b6b61
SanXiaoXing <87142318+SanXiaoXing@users.noreply.github.com>
2023-06-25 11:18:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_cXM6TJkduGYjkM6pgNYWzqQjAU', 
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EDHogCTMikWDMM0RFh87sxOalgQ4_Q0XhgRS6Z-YyLE',
+      useTemplateId: 'Q4yyNQlwMDc9wlsbz6ykkWMT3oiZGBPI-HKbJVt0TEI',
       province: '北京',
       city: '北京',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
     {
       name: '蔡文博',
       id: 'o_cXM6TeI7sGXQsRYt6LGJjz5CE4',
-      useTemplateId: 'EDHogCTMikWDMM0RFh87sxOalgQ4_Q0XhgRS6Z-YyLE',
+      useTemplateId: 'Q4yyNQlwMDc9wlsbz6ykkWMT3oiZGBPI-HKbJVt0TEI',
       province: '河南',
       city: '安阳',
     },
