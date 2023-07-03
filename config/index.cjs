32d5290a94aff8e67e04ca39f38a7d7190aae25f
SanXiaoXing <87142318+SanXiaoXing@users.noreply.github.com>
2023-07-03 17:46:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2000', date: '07-03',
+          type: '*生日', name: 'SanXiaoXing', year: '2000', date: '07-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2000', date: '08-02',
+          type: '生日', name: 'SanXiaoXing', year: '2000', date: '08-02',
         },
         {
           type: '节日', name: '工作时间', year: '2023', date: '06-25',
